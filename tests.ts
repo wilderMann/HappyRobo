9dcfc282da2e52473cfa25f5007b51fa556f1177
a.wild.man <andreas.wildmann@gmail.com>
2024-04-09 14:52:17 +0200

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,3 +1,19 @@
+happyrobo.initMCP23017LED(Ports.GPA7)
+MCP23017.initMCP23017Button(Ports.GPB7)
+basic.forever(function () {
+    MCP23017.setLed(Ports.GPB0, true)
+    basic.pause(500)
+    MCP23017.setLed(Ports.GPB0, false)
+    basic.pause(500)
+})
+basic.forever(function () {
+    if (MCP23017.MCPButtonPressed(Ports.GPB7)) {
+        led.plot(2, 2)
+    } else {
+        led.unplot(2, 2)
+    }
+})
+
 // go right 90 degrees
 input.onButtonPressed(Button.A, () => {
     motion_kit.turnRight(90);
