2b5ab328d4e7028afcba995ac75451225c8442f4
a.wild.man <andreas.wildmann@gmail.com>
2024-04-09 14:54:54 +0200

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,13 +1,13 @@
 happyrobo.initMCP23017LED(Ports.GPA7)
-MCP23017.initMCP23017Button(Ports.GPB7)
+happyrobo.initMCP23017Button(Ports.GPB7)
 basic.forever(function () {
-    MCP23017.setLed(Ports.GPB0, true)
+    happyrobo.setLed(Ports.GPB0, true)
     basic.pause(500)
-    MCP23017.setLed(Ports.GPB0, false)
+    happyrobo.setLed(Ports.GPB0, false)
     basic.pause(500)
 })
 basic.forever(function () {
-    if (MCP23017.MCPButtonPressed(Ports.GPB7)) {
+    if (happyrobo.MCPButtonPressed(Ports.GPB7)) {
         led.plot(2, 2)
     } else {
         led.unplot(2, 2)
@@ -16,14 +16,14 @@ basic.forever(function () {
 
 // go right 90 degrees
 input.onButtonPressed(Button.A, () => {
-    motion_kit.turnRight(90);
+    happyrobo.turnRight(90);
 })
 // go forward 10
 input.onButtonPressed(Button.B, () => {
-    motion_kit.driveForwards(10);
+    happyrobo.driveForwards(10);
 })
 // stop
 input.onButtonPressed(Button.AB, () => {
-    motion_kit.stop
+    happyrobo.stop
 })
 
